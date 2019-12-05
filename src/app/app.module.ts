9ba3bd8c9561fc464bf287949bdc65a35e06fca5
Stonebank <pratikdevlekar786@gmail.com>
2019-12-06 02:10:29 +0530

    add file server.component and update app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { servercomponent} from './server/server.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    servercomponent,
   ],
   imports: [
     BrowserModule
